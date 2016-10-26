f5184daabd773d4a9d77f8ccdfa71d9fb4517e65
studerande <studerande>
2016-10-26 10:58:57 +0200

    La till google för mig

---
+++ Jenpbiz/Jenpbiz/Controllers/GoogleAPIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jenpbiz.Controllers
+{
+    public class GoogleAPIController : Controller
+    {
+        // GET: GoogleAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
