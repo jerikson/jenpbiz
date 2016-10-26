2b15de924f4573c53144be2a5d678851fb31a6fa
Jimmy Erikson <jimmyerikson@gmail.com>
2016-10-26 10:57:16 +0200

    Modifed API.js, AJAX calling Product/GetMerchantProduct, - getting internal server error due to broken debug mode, app quitting..

--- Jenpbiz/Jenpbiz/Scripts/API.js
+++ Jenpbiz/Jenpbiz/Scripts/API.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(document).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: "/Product/GetMerchantProduct",
+            dataType: 'json',
+            success: function (data) {
+                console.log('success')
+                data = JSON.parse(data);
+                data.response.games.forEach(function (p) {
+                    $('#placeholder').append('<li>' + p.productId + '</li>');
+                });
+            }
+        });
+    });
