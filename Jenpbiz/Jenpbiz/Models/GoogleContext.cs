1474eafe93264cbbc88dd126209974620a7aa61d
jerikson <jimmyerikson@gmail.com>
2017-05-15 16:32:45 +0200

    Update GoogleContext.cs

--- Jenpbiz/Jenpbiz/Models/GoogleContext.cs
+++ Jenpbiz/Jenpbiz/Models/GoogleContext.cs
@@ -56,25 +56,3 @@ namespace Jenpbiz.Models
 
     }
 }
-
-//string ConnString = Utils.GetConnString();
-//string SqlString = "Select * From Contacts Where FirstName = ? And LastName = ?";
-//        using (OleDbConnection conn = new OleDbConnection(ConnString))
-//        {
-//          using (OleDbCommand cmd = new OleDbCommand(SqlString, conn))
-//          {
-//            cmd.CommandType = CommandType.Text;
-//            cmd.Parameters.AddWithValue("FirstName", txtFirstName.Text);
-//            cmd.Parameters.AddWithValue("LastName", txtLastName.Text);
- 
-//            conn.Open();
-//            using (OleDbDataReader reader = cmd.ExecuteReader())
-//            {
-//              while (reader.Read())
-//              {
-//                Response.Write(reader["FirstName"].ToString() + " " + reader["LastName"].ToString());
-//              }
-//            }
-//          }
-//        }
- 
